1527790beee01b4628ab3dd9e83f626bf08d89bd
rebornwhu <rebornwhu@gmail.com>
2015-04-18 13:26:59 -0700

    first commit

---
+++ BasicInteraction/ViewController.m
@@ -0,0 +1,45 @@
+//
+//  ViewController.m
+//  BasicInteraction
+//
+//  Created by Xiao Lu on 4/18/15.
+//  Copyright (c) 2015 Xiao Lu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *simpleTextField;
+@property (weak, nonatomic) IBOutlet UILabel *simpleLabel;
+
+@end
+
+@implementation ViewController
+
+- (IBAction)changeLabel:(id)sender {
+    NSString *contents = self.simpleTextField.text;
+    
+    NSString *message = [NSString stringWithFormat:@"Hello, %@", contents];
+    [self.simpleLabel setText:message];
+    
+    [self.simpleTextField resignFirstResponder];
+}
+
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    [self.view endEditing:YES];
+}
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
